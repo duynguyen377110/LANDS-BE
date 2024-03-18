74b7df6533bd7b544f794dd9515f36a1b455a833
duy366110 <duy366110@gmail.com>
2024-03-18 15:46:35 +0700

    #Config swagger in my backend.

--- environment.js
+++ environment.js
@@ -1,6 +1,23 @@
 "use strict"
 const environment = {
+    model: {
+        dev: {
+            cors: {
+                origins: [
+                    'http://localhost:4200',
+                    'http://localhost:8080'
+                ]
+            }
+        },
+        pro: {
+
+        }
+    },
     port: 8080
 }
 
+const env = environment.model.dev;
+
 module.exports = environment;
+
+module.exports.model = env;
---
+++ src/middleware/middleware-cors.js
@@ -0,0 +1,25 @@
+const model = require("../../environment").model;
+
+class MiddlewareCors  {
+
+    constructor() { }
+
+    cors = (req, res, next) => {
+        let origin = req.get('origin');
+
+        // if(model.cors.origins.some((host) => host === origin)) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            next();
+
+        // } else {
+        //     res.status(400).json({status: false, message: 'Corss origin resuce sharing policy from server'});
+
+        // }
+        
+    }
+
+}
+
+module.exports = new MiddlewareCors();
