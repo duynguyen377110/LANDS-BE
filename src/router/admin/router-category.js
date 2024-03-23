1ebfea9fb8aded34f378b3ae81effa668f158e07
duy366110 <duy366110@gmail.com>
2024-03-23 22:53:09 +0700

    #Commit 23.03.2024: Config service get amount category.

--- src/router/admin/router-category.js
+++ src/router/admin/router-category.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ControllerCategory = require("../../controller/controller-category");
 const router = express.Router();
 
+router.get('/amount', ControllerCategory.getAmount)
 router.post('/new', ControllerCategory.createCategory);
 
 module.exports = router;
