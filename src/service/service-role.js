0b28bf76158eef91d102a12e24e00c93ff09e3c6
duy366110 <duy366110@gmail.com>
2024-03-30 23:52:13 +0700

    #Commit 30.03.2024: COnfig server delete role.

--- src/service/service-role.js
+++ src/service/service-role.js
@@ -6,6 +6,18 @@ class ServiceRole {
     constructor() { }
 
     /**
+     * GET ALL ROLE
+     * @returns 
+     */
+    async getAllRole() {
+        try {
+            return await ModelRole.find({}).lean();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
      * CREATE ROLE
      * @param {*} infor 
      * @returns 
@@ -13,6 +25,20 @@ class ServiceRole {
     async createRole(infor = {}) {
         try {
             return await ModelRole.create({title: infor.title});
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+     * DELETE ROLE
+     * @param {*} infor 
+     * @returns 
+     */
+    async deleteRole(infor = {}) {
+        try {
+            await ModelRole.findOneAndDelete({_id: {$eq: infor.id}});
+            return { status: true, message: 'Delete role success'};
         } catch (error) {
             throw error;
         }
