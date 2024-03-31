b517d132a5fa2c5f1a1e03fa227834ecc739b552
duy366110 <duy366110@gmail.com>
2024-03-31 09:59:56 +0700

    #Config service update role.

--- src/router/common/common-router-role.js
+++ src/router/common/common-router-role.js
@@ -4,5 +4,6 @@ const ControllerRole = require("../../controller/controller-role");
 const router = express.Router();
 
 router.get("/all", ControllerRole.getAllRole);
+router.get("/:id", ControllerRole.getRoleById);
 
 module.exports = router;
--- src/service/service-role.js
+++ src/service/service-role.js
@@ -18,6 +18,32 @@ class ServiceRole {
     }
 
     /**
+     * GET ROLE BY ID
+     * @param {*} id 
+     * @returns 
+     */
+    async getRoleById(id = '') {
+        try {
+            return await ModelRole.findById(id).lean();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+     * FIND ROLE BY ID
+     * @param {*} id 
+     * @returns 
+     */
+    async findRoleById(id = '') {
+        try {
+            return await ModelRole.findById(id);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
      * CREATE ROLE
      * @param {*} infor 
      * @returns 
@@ -25,6 +51,27 @@ class ServiceRole {
     async createRole(infor = {}) {
         try {
             return await ModelRole.create({title: infor.title});
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+     * UPDATE ROLE
+     * @param {*} infor 
+     * @returns 
+     */
+    async updateRole(infor = {}) {
+        try {
+            let role = await this.findRoleById(infor.id);
+            
+            if(!role) {
+                return {status: false, message: 'Update role unsuccess'};
+            }
+            role.title = infor.title;
+            await role.save();
+            return {status: true, message: 'Update role success'};
+
         } catch (error) {
             throw error;
         }
