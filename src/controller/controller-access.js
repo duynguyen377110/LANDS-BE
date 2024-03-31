b9c8ee71f764eae3a7c1c7e5a597b2975a20229d
duy366110 <duy366110@gmail.com>
2024-03-31 22:27:42 +0700

    #Config service user signout.

--- src/controller/controller-access.js
+++ src/controller/controller-access.js
@@ -9,6 +9,13 @@ class ControllerAccess {
         return res.status(200).json("User signup success");
     }
 
+    /**
+     * ADMIN SIGNIN
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @returns 
+     */
     async adminSignin(req, res, next) {
         let { email, password } = req.body;
         let { status, message, access } = await ServiceAccess.userSignin({email, password});
@@ -29,6 +36,24 @@ class ControllerAccess {
                 refreshToken: access.refreshToken
             }
         });
+    }
+
+    /**
+     * ADMIN SIGNOUT
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @returns 
+     */
+    async adminSignout(req, res, next) {
+        let { email } = req.body;
+        let { status, message } = await ServiceAccess.userSignout({email});
+
+        if(!status) {
+            return res.status(400).json({status, message});
+        }
+
+        return res.status(200).json({status, message});
     }
 
 }
