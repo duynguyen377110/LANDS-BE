381761955df4c78dc41edf5f7fba19f4e6c0007e
duy366110 <duy366110@gmail.com>
2024-03-31 16:00:53 +0700

    #Commit service update user account.

--- src/router/common/common-router-user.js
+++ src/router/common/common-router-user.js
@@ -4,6 +4,6 @@ const ControllerUser = require("../../controller/controller-user");
 const router = express.Router();
 
 router.get("/all", ControllerUser.getAll);
-
+router.get("/:id", ControllerUser.getUserById);
 
 module.exports = router;
