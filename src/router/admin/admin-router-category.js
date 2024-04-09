2d503d5efeb9a070581df5abdec059e5f25e1172
duy366110 <duy366110@gmail.com>
2024-04-09 15:13:34 +0700

    #COnfig service update category.

--- src/router/admin/admin-router-category.js
+++ src/router/admin/admin-router-category.js
@@ -1,11 +1,38 @@
 "use strict"
 const express = require("express");
+const validator = require("express-validator");
 const ControllerCategory = require("../../controller/controller-category");
 const MiddlewareException = require("../../middleware/middleware-exception");
 const router = express.Router();
 
-router.post('/', MiddlewareException.except(ControllerCategory.createCategory));
-router.patch('/', MiddlewareException.except(ControllerCategory.uploadCategoryThumb));
-router.delete('/', MiddlewareException.except(ControllerCategory.deleteCategry));
+router.post('/', [
+    validator
+    .check("title").notEmpty()
+    .withMessage("Title not empty"),
+
+    validator
+    .check("description").notEmpty()
+    .withMessage("Description not empty"),
+], MiddlewareException.except(ControllerCategory.createCategory));
+
+router.patch('/',[
+    validator
+    .check("id").notEmpty()
+    .withMessage("Id not empty"),
+
+    validator
+    .check("title").notEmpty()
+    .withMessage("Title not empty"),
+
+    validator
+    .check("description").notEmpty()
+    .withMessage("Description not empty"),
+], MiddlewareException.except(ControllerCategory.updateCategory));
+
+router.delete('/',[
+    validator
+    .check("id").notEmpty()
+    .withMessage("Id not empty"),
+], MiddlewareException.except(ControllerCategory.deleteCategry));
 
 module.exports = router;
