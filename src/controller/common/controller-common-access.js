dcb3fe63c1034f0e15b6ab66c7ffd85e82885e02
duy366110 <duy366110@gmail.com>
2024-04-14 16:30:05 +0700

    #COnfig service signin - signup -signout.

--- src/controller/common/controller-common-access.js
+++ src/controller/common/controller-common-access.js
@@ -1,4 +1,5 @@
 "use strict"
+const { validationResult } = require("express-validator");
 const getCloud = require("../../amqp/amqp-core").getCloud;
 const configQueue = require("../../config/config-queue");
 const AmqpProducer = require("../../amqp/amqp-reducer");
@@ -17,6 +18,9 @@ class ControllerCommonAccess {
      * @returns 
      */
     async signup(req, res, next) {
+        const error = validationResult(req);
+        if (!error.isEmpty()) throw new BadRequestError(error.array()[0].msg)
+
         let CONNECT = getCloud();
         let REDUCER = configQueue.AUTH.SIGNUP.REDUCER_SIGNUP;
         let CONSUMER = configQueue.AUTH.SIGNUP.COMSUMER_SIGNUP;
@@ -32,6 +36,41 @@ class ControllerCommonAccess {
     }
 
     /**
+     * CLIENT SIGNIN ACCOUNT
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @returns 
+     */
+    async signin(req, res, next) {
+        const error = validationResult(req);
+        if (!error.isEmpty()) throw new BadRequestError(error.array()[0].msg)
+
+        let CONNECT = getCloud();
+        let REDUCER_SIGNIN = configQueue.AUTH.SIGNIN.REDUCER_SIGNIN;
+        let CONSUMER = configQueue.AUTH.SIGNIN.COMSUMER_SIGNIN;
+        let { email, password } = req.body;
+
+        await AmqpProducer.producer(CONNECT, REDUCER_SIGNIN, JSON.stringify({email, password}));
+        return await AmqpConsumer.consumer(CONNECT, CONSUMER, (information) => {
+            let { status, message, access } = information;
+
+            if(!status) throw new BadRequestError(message)
+
+            let metadata = {
+                userId: access.user._id,
+                email: access.user.email,
+                phone: access.user.phone,
+                address: access.user.address,
+                accessToken: access.accessToken,
+                refreshToken: access.refreshToken
+            }
+
+            new Ok(message).response(res, metadata);
+        })
+    }
+
+    /**
      * CLIENT SIGNOUT ACCOUNT
      * @param {*} req 
      * @param {*} res 
@@ -40,13 +79,12 @@ class ControllerCommonAccess {
      */
     async signout(req, res, next) {
         const error = validationResult(req);
-        let { email } = req.body;
-
         if (!error.isEmpty()) throw new BadRequestError(error.array()[0].msg)
 
         let CONNECT = getCloud();
         let REDUCER_SIGNOUT = configQueue.AUTH.SIGNOUT.REDUCER_SIGNOUT;
         let CONSUMER = configQueue.AUTH.SIGNOUT.COMSUMER_SIGNOUT;
+        let { email } = req.body;
 
         await AmqpProducer.producer(CONNECT, REDUCER_SIGNOUT, JSON.stringify({email}));
         return await AmqpConsumer.consumer(CONNECT, CONSUMER, (information) => {
