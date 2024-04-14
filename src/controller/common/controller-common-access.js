5bf606dfbc4b8640940c4964b5274bf40da1a2f4
duy366110 <duy366110@gmail.com>
2024-04-14 15:46:08 +0700

    #Config service signup and signout.

--- src/controller/common/controller-common-access.js
+++ src/controller/common/controller-common-access.js
@@ -9,6 +9,13 @@ class ControllerCommonAccess {
 
     constructor() { }
 
+    /**
+     * CLIENT SIGNUP ACCOUNT
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @returns 
+     */
     async signup(req, res, next) {
         let CONNECT = getCloud();
         let REDUCER = configQueue.AUTH.SIGNUP.REDUCER_SIGNUP;
@@ -21,6 +28,32 @@ class ControllerCommonAccess {
 
             if(!status) throw new BadRequestError(message)
             new Ok(message).response(res, {access});
+        })
+    }
+
+    /**
+     * CLIENT SIGNOUT ACCOUNT
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @returns 
+     */
+    async signout(req, res, next) {
+        const error = validationResult(req);
+        let { email } = req.body;
+
+        if (!error.isEmpty()) throw new BadRequestError(error.array()[0].msg)
+
+        let CONNECT = getCloud();
+        let REDUCER_SIGNOUT = configQueue.AUTH.SIGNOUT.REDUCER_SIGNOUT;
+        let CONSUMER = configQueue.AUTH.SIGNOUT.COMSUMER_SIGNOUT;
+
+        await AmqpProducer.producer(CONNECT, REDUCER_SIGNOUT, JSON.stringify({email}));
+        return await AmqpConsumer.consumer(CONNECT, CONSUMER, (information) => {
+            let { status, message } = information;
+
+            if(!status) throw new BadRequestError(message)
+            return new Ok(message).response(res);
         })
     }
 
--- src/router/common/common-router-access.js
+++ src/router/common/common-router-access.js
@@ -1,8 +1,18 @@
 "use strict"
 const express = require('express');
+const validator = require("express-validator");
 const ControllerCommonAccess = require("../../controller/common/controller-common-access");
+const MiddlewareException = require("../../middleware/middleware-exception");
 const router = express.Router();
 
-router.post('/signup', ControllerCommonAccess.signup);
+router.post('/signup', MiddlewareException.except(ControllerCommonAccess.signup));
+router.post('/signout',[
+    validator
+    .check("email")
+    .notEmpty()
+    .withMessage("E-mail not empty")
+    .isEmail()
+    .withMessage("E-mail invalid"),
+], MiddlewareException.except(ControllerCommonAccess.signout));
 
 module.exports = router;
